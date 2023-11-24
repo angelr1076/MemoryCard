a059aa4390c4c77b591b9d2480579f548660d793
angelr1076 <angelr1076@gmail.com>
2023-11-24 10:34:47 -0500

    Created splash page for game

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,17 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import cards from './assets/images/cards.svg';
+import './assets/styles/style.css';
 
 function App() {
   return (
     <>
       <div>
-        <a href='https://vitejs.dev' target='_blank' rel='noreferrer'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank' rel='noreferrer'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
+        <div className='card-container'>
+          <img src={cards} className='card-logo' alt='Card logo' />
+        </div>
       </div>
       <h1>Memory Card</h1>
       <div className='card'>
-        <button>Play</button>
+        <button className='play btn'>Play</button>
         <p>
           Click on &quot;Play&quot; to start the game. <br />
         </p>
